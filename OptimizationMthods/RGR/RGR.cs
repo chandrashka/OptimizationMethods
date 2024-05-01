c20f2b3ee347afa380434c9aa35b44b0647c65eb
Yuliia <julia10942@gmail.com>
2024-05-01 14:08:23 +0200

    Fixed Dsk paul method

--- OptimizationMthods/RGR/RGR.cs
+++ OptimizationMthods/RGR/RGR.cs
@@ -4,18 +4,16 @@ public class RGR
 {
     public const int Accuracy = 3;
     public const double E = 0.01;
-    //public static readonly double[,] H = {{6,1}, {1,14}};
+
     public static readonly double[,] H = { { 6, -1 }, { -1, 8 } };
     
     private static double F(double x1, double x2)
     {
-        //return Math.Round(3*Math.Pow(x1-3, 2) + x1 * x2 + 7*Math.Pow(x2, 2), Accuracy);
         return Math.Round(3 * Math.Pow(x1 - 1, 2) - x1 * x2 + 4 * Math.Pow(x2, 2), Accuracy);
     }
     
     private static double[] Gradient(double x1, double x2)
     {
-        //return [Math.Round(6 * x1 + x2 - 18, Accuracy), Math.Round(x1 + 14 * x2, Accuracy)];
         return [Math.Round(6 * x1 - x2 - 6 , Accuracy), Math.Round(- x1 + 8 * x2, Accuracy)];
     }
     
@@ -34,7 +32,7 @@ public class RGR
         var deltaL = FindDeltaL(x0, s2);
         var interval = Sven(0, deltaL, x0, s2, out var lt);
 
-        var gd = GoldCut(interval, E, x0, s2);
+        GoldCut(interval, E, x0, s2);
 
         var l1 = FindL(x0, s2);
         double[] x1 = [x0[0] + l1 * s2[0], x0[1] + l1 * s2[1]];
@@ -44,7 +42,7 @@ public class RGR
         deltaL = FindDeltaL(x1, s1);
         interval = Sven(0, deltaL, x1, s1, out lt);
 
-        gd = GoldCut(interval, E, x1, s1);
+        GoldCut(interval, E, x1, s1);
 
         var l2 = FindL(x1, s1);
         double[] x2 = [x1[0] + l2 * s1[0], x1[1] + l2 * s1[1]];
@@ -53,30 +51,19 @@ public class RGR
         Console.WriteLine($"f(x2) - f(x1) = {F(x2)} - {F(x1)} = {F(x2) - F(x1)} <= {E}   {Math.Round(Math.Abs(F(x2) - F(x1)),Accuracy) <= E}");
 
         deltaL = FindDeltaL(x2, s2);
-        interval = Sven(0, deltaL, x2, s2, out var l3, true);
+        Sven(0, deltaL, x2, s2, out var l3, true);
         double[] x3 = [x2[0] + l3 * s2[0], x2[1] + l3 * s2[1]];
         x3 = [Math.Round(x3[0], Accuracy), Math.Round(x3[1], Accuracy)];
         Console.WriteLine($"\nx3 = ({Print(x2)})^T + {l3} * ({Print(s2)})^T = ({Print(x3)})^T");
         Console.WriteLine($"f(x3) - f(x2) = {F(x3)} - {F(x2)} = {Math.Round(F(x3) - F(x2),Accuracy)} <= {E}   {Math.Round(Math.Abs(F(x3) - F(x2)),Accuracy) <= E}");
-        
-        deltaL = FindDeltaL(x3, [x3[0] - x1[0], x3[1] - x1[1]]);
-        interval = ShortSven(0, deltaL, x3, s2, out var l4, true);
-        double[] x4 = [x3[0] + l4 * x3[0] - x1[0], x3[1] + l4 * x3[1] - x1[1]];
-        x4 = [Math.Round(x4[0], Accuracy), Math.Round(x4[1], Accuracy)];
-        Console.WriteLine($"x4 = ({Print(x3)})^T + {l4} * ({Print([x3[0] - x1[0], x3[1] - x1[1]])})^T = ({Print(x4)})^T");
-        Console.WriteLine($"f(x4) - f(x3) = {F(x4)} - {F(x3)} = {F(x4) - F(x3)} <= {E}   {F(x4) - F(x3) <= E}");
-        
-        // deltaL = FindDeltaL(x3, s1);
-        // interval = Sven(0, deltaL, x3, s1, out var l4, true);
-        // double[] x4 = [x3[0] + l4 * s1[0], x3[1] + l4 * s1[1]];
-        // x4 = [Math.Round(x4[0], Accuracy), Math.Round(x4[1], Accuracy)];
-        // Console.WriteLine($"x4 = ({Print(x3)})^T + {l4} * ({Print(s1)})^T = ({Print(x4)})^T");
-        // Console.WriteLine($"f(x4) - f(x3) = {F(x4)} - {F(x3)} = {Math.Round(F(x4) - F(x3), Accuracy)} <= {E}   {Math.Round(Math.Abs(F(x4) - F(x3)),Accuracy) <= E}");
 
-        Console.WriteLine(Print(x1));
-        Console.WriteLine(Print(x2));
-        Console.WriteLine(Print(x3));Console.WriteLine(Print(x4));
-        
+        double[] direction = [Math.Round(x3[0] - x1[0], Accuracy), Math.Round(x3[1] - x1[1], Accuracy)];
+        deltaL = FindDeltaL(x3, direction);
+        ShortSven(0, deltaL, x3, s2, out var l4, true);
+        double[] x4 = [x3[0] + l4 * direction[0], x3[1] + l4 * direction[1]];
+        x4 = [Math.Round(x4[0], Accuracy), Math.Round(x4[1], Accuracy)];
+        Console.WriteLine($"x4 = ({Print(x3)})^T + {l4} * ({Print(direction)})^T = ({Print(x4)})^T");
+        Console.WriteLine($"f(x4) - f(x3) = {F(x4)} - {F(x3)} = {F(x4) - F(x3)} <= {E}   {F(x4) - F(x3) <= E}");
     }
 
     private static string Print(double[] x)
@@ -161,7 +148,7 @@ public class RGR
         x[^1] = middle;
         x.Add(temp);
         
-        var fmid = Math.Round(F(xk, s, middle), 2);
+        var fmid = Math.Round(F(xk, s, middle), Accuracy);
         temp = values[^1];
         values[^1] = fmid;
         values.Add(temp);
@@ -187,61 +174,100 @@ public class RGR
         interval[1] = Math.Round(interval[1], Accuracy);
         Console.WriteLine($"Iнтервал невизначеностi: [{Print(interval)}]\n");
         
-        ls = 0; 
-        if (isLast)
-        {
-            Console.WriteLine("Метод ДСК Пауелла");
-            List<double> tempValues = new List<double>();
-            foreach (var value in values)
-            {
-                tempValues.Add(value);
-            }
-            tempValues.Sort();
-            tempValues = new List<double>() { tempValues[0], tempValues[1], tempValues[2]};
+        FindXS(out ls, isLast, interval, s, xk);
 
-            double l1 = 0;
-            double l2 = 0;
-            double l3 = 0;
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l1 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l2 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l3 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            
-            var top = step * (l1 - l3);
-            var bot = 2 * (l1 - 2 * l2 + l3);
-            var l = step + top / bot;
-            ls = Math.Round(l,Accuracy);
-            Console.WriteLine($"x1 = {l1}  x2 = {l2}  x3 = {l3}");
-            Console.WriteLine($"l = {ls}");
-        }
-        
         return interval;
     }
 
-    private static double[] ShortSven(double l0, double step, double[] xk, double[] s, out double ls, bool isLast = false)
+    private static void FindXS(out double ls, bool isLast, double[] interval, double[] s, double[] xk)
+    {
+        ls = 0;
+        double xs = 0;
+        if (isLast)
+        {
+            Console.WriteLine("Метод ДСК Пауелла");
+
+            var x1 = interval[0];
+            var x2 = (interval[0] + interval[1]) / 2;
+            var x3 = interval[1];
+
+            double fx1 = Math.Round(F(xk, s, x1, false), Accuracy);
+            double fx2 = Math.Round(F(xk, s, x2, false), Accuracy);
+            double fx3 = Math.Round(F(xk, s, x3, false), Accuracy);
+            
+            var delta = x3-x2;
+            
+            var top = delta * (fx1 - fx3);
+            var bot = 2 * (fx1 - 2 * fx2 + fx3);
+            var x = x2 + top / bot;
+            xs = Math.Round(x,Accuracy);
+            var fxs = F(xk, s, xs, false);
+            Console.WriteLine($"x1 = {x1}  x2 = {x2}  x3 = {x3}");
+            Console.WriteLine($"x* = {xs}  fx* = {fxs}");
+            
+            Console.WriteLine("Виконується перевiрка на закiнчення пошуку: ");
+            Console.WriteLine($"|fx2 - fx*| <= e");
+            Console.WriteLine($"|x2 - x*| <= e");
+
+            var result = Math.Abs(fx2 - fxs) <= E && Math.Abs(x2 - xs) <= E;
+            Console.WriteLine($"|{fx2} - {fxs}| <= {E}    {Math.Abs(fx2-fxs) <= E}");
+            Console.WriteLine($"|{x2} - {xs}| <= {E}       {Math.Abs(x2-xs) <= E}");
+
+            if (result)
+            {
+                Console.WriteLine($"x* = {xs}");
+                ls = xs;
+            }
+            else
+            {
+                Console.WriteLine("Умови закiнчення пошуку не виконуються.");
+                List<double> values = new List<double>() { fxs, fx1, fx2, fx3 };
+                List<double> listx = new List<double>() { xs, x1, x2, x3 };
+                var max = FindMax(values);
+                var indexMax = values.IndexOf(max);
+                values.Remove(max);
+                listx.RemoveAt(indexMax);
+
+                var min = FindMin(values);
+                var indexMin = values.IndexOf(min);
+
+                x2 = listx[indexMin];
+                fx2 = min;
+                values.Remove(min);
+                listx.RemoveAt(indexMin);
+
+                if (values[0] <= values[1])
+                {
+                    x1 = listx[0];
+                    fx1 = values[0];
+                    x3 = listx[1];
+                    fx3 = values[1];
+                }
+                else
+                {
+                    x1 = listx[1];
+                    fx1 = values[1];
+                    x3 = listx[0];
+                    fx3 = values[0];
+                }
+                
+                var a1 = (fx2 - fx1) / x2 - x1;
+                a1 = Math.Round(a1, Accuracy);
+                Console.WriteLine($"a1 = {a1}");
+
+                var a2 = (1 / (x3 - x2)) * ((fx3-fx2)/(x3-x2) - (fx2-fx1)/(x2-x1));
+                a2 = Math.Round(a2, Accuracy);
+                Console.WriteLine($"a2 = {a2}");
+
+                xs = (x1 + x2) / 2 - (a1 / (2 * a2));
+                xs = Math.Round(xs, Accuracy);
+                Console.WriteLine($"x* = ({x1} + {x2}) / 2 - {a1} / {2*a2} = {xs}");
+                ls = xs;
+            }
+        }
+    }
+
+    private static void ShortSven(double l0, double step, double[] xk, double[] s, out double ls, bool isLast = false)
     {
         Console.WriteLine("Алгоритм Свена");
         var fx0 = F(xk, s, l0);
@@ -267,58 +293,7 @@ public class RGR
         interval[1] = Math.Round(interval[1], Accuracy);
         Console.WriteLine($"Iнтервал невизначеностi: [{Print(interval)}]\n");
         
-        ls = 0; 
-        if (isLast)
-        {
-            Console.WriteLine("Метод ДСК Пауелла");
-            List<double> tempValues = new List<double>();
-            foreach (var value in values)
-            {
-                tempValues.Add(value);
-            }
-            tempValues.Sort();
-            tempValues = new List<double>() { tempValues[0], tempValues[1], tempValues[2]};
-
-            double l1 = 0;
-            double l2 = 0;
-            double l3 = 0;
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l1 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l2 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            foreach (var value in values)
-            {
-                if (tempValues.Contains(value))
-                {
-                    l3 = value;
-                    tempValues.Remove(value);
-                    break;
-                }
-            }
-            
-            var top = step * (l1 - l3);
-            var bot = 2 * (l1 - 2 * l2 + l3);
-            var l = step + top / bot;
-            ls = Math.Round(l,Accuracy);
-            Console.WriteLine($"x1 = {l1}  x2 = {l2}  x3 = {l3}");
-            Console.WriteLine($"l = {ls}");
-        }
-        
-        return interval;
+        FindXS(out ls, isLast, interval, s, xk);
     }
 
     public static double F(double[] xn)
@@ -334,12 +309,10 @@ public class RGR
         return F(x1,x2);
     }
 
-    
-
-    private static double[] GoldCut(double[] startInterval, double e, double[] xk, double[] s)
+    private static void GoldCut(double[] startInterval, double e, double[] xk, double[] s)
     {
         Console.WriteLine("Метод золотого перетину");
-        if (CheckInterval(startInterval, e)) return startInterval;
+        if (CheckInterval(startInterval, e)) return;
 
         var newInterval = startInterval;
 
@@ -371,8 +344,6 @@ public class RGR
         }
                 
         Console.WriteLine($"Iнтервал: [{Print(newInterval)}]");
-        
-        return newInterval;
     }
 
     private static double FindX2(IReadOnlyList<double> startInterval, double l)
@@ -389,5 +360,31 @@ public class RGR
     {
         var l = startInterval[1] - startInterval[0];
         return l <= e;
+    }
+    
+    static double FindMax(List<double> numbers)
+    {
+        double max = numbers[0];
+        foreach (double number in numbers)
+        {
+            if (number >= max)
+            {
+                max = number;
+            }
+        }
+        return max;
+    }
+    
+    static double FindMin(List<double> numbers)
+    {
+        double min = numbers[0];
+        foreach (double number in numbers)
+        {
+            if (number <= min)
+            {
+                min = number;
+            }
+        }
+        return min;
     }
 }
--- OptimizationMthods/obj/Debug/net8.0/OptimizationMthods.AssemblyInfo.cs
+++ OptimizationMthods/obj/Debug/net8.0/OptimizationMthods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizationMthods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd22176d0a53b6d7b2b5ce30aa31e6516c8fa0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizationMthods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizationMthods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
